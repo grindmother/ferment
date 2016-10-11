7d7478870dffefd158e2f78e0269c7714a01e52d
Matt McKegg <matt@wetsand.co.nz>
2016-10-11 20:52:51 +1300

    when there are no posts on discover feed, show new user welcome

--- views/discovery-feed.js
+++ views/discovery-feed.js
@@ -5,6 +5,7 @@ var when = require('@mmckegg/mutant/when')
 var renderMiniProfile = require('../widgets/mini-profile')
 var computed = require('@mmckegg/mutant/computed')
 var send = require('@mmckegg/mutant/send')
+var Path = require('path')
 
 module.exports = DiscoveryFeed
 
@@ -13,14 +14,35 @@ function DiscoveryFeed (context) {
 
   var suggestedProfiles = context.suggestedProfiles
   var suggestedProfilesCount = computed(suggestedProfiles, x => x.length)
+  var postCount = computed(context.discoveryFeed, x => x.length)
 
   return h('Feed', [
     h('div.main', [
       h('h1', 'Discovery Feed'),
       when(context.discoveryFeed.sync,
-        MutantMap(context.discoveryFeed, (item) => renderAudioPost(context, item), {
-          maxTime: 5
-        }),
+        when(postCount,
+          MutantMap(context.discoveryFeed, (item) => renderAudioPost(context, item), {
+            maxTime: 5
+          }),
+          h('div', { style: { 'font-size': '120%' } }, [
+            h('strong', `There's nothing to see here yet.`),
+            h('p', [
+              `You can start adding music to `,
+              h('a', { href: '#', 'ev-click': send(context.actions.viewProfile, context.api.id) }, `your profile`),
+              `, or `,
+              h('a', { href: '#', 'ev-click': context.actions.openJoinPubWindow }, `follow a pub server`),
+              ` to really get things going! 🍻`
+            ]),
+            h('img', {
+              src: `file://${Path.join(__dirname, '..', 'ferment-logo.png')}`,
+              style: {
+                display: 'block',
+                width: 200,
+                margin: '60px auto'
+              }
+            })
+          ])
+        ),
         h('Loading -large')
       )
     ]),
