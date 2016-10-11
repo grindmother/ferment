d57ac8b07da35f404d7db4289dca6c5cde924167
Matt McKegg <matt@wetsand.co.nz>
2016-10-11 18:06:51 +1300

    use prebuilt ffmpeg on macOS

---
+++ bin/build-ffmpeg.sh
@@ -0,0 +1,34 @@
+./configure --enable-static --disable-shared --disable-all --enable-ffmpeg --enable-avcodec --enable-avformat --enable-avutil \
+  --enable-swresample --enable-swscale --enable-avfilter \
+  --disable-network --disable-d3d11va --disable-dxva2 --disable-vaapi --disable-vda --disable-vdpau \
+  --enable-decoder=mp3 \
+  --enable-decoder=aac \
+  --enable-decoder=flac \
+  --enable-decoder=opus \
+  --enable-decoder=vorbis \
+  --enable-decoder=pcm_f32le  --enable-decoder=pcm_f64le  --enable-decoder=pcm_s16le  --enable-decoder=pcm_s24le  --enable-decoder=pcm_s32le \
+  --enable-demuxer=flac \
+  --enable-demuxer=matroska \
+  --enable-demuxer=mp3 \
+  --enable-demuxer=aiff	\
+  --enable-demuxer=ogg \
+  --enable-demuxer=wav \
+  --enable-demuxer=data \
+  --enable-protocol=file \
+  --enable-protocol=pipe \
+  --disable-bzlib \
+  --disable-iconv \
+  --disable-libxcb \
+  --disable-lzma \
+  --disable-sdl \
+  --disable-securetransport \
+  --disable-xlib \
+  --disable-zlib \
+  --enable-filter=aresample \
+  --enable-encoder=libopus \
+  --enable-encoder=pcm_s8 \
+  --enable-muxer=pcm_s8 \
+  --enable-muxer=webm \
+  --enable-muxer=segment --enable-libopus && make
+
+# build for windows: https://github.com/rdp/ffmpeg-windows-build-helpers
